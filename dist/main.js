088feae77bac4cbd190e11647198fa8f94e4fa05
Cormorant Programming <cormorant.programming@gmail.com>
2021-08-15 09:28:38 +0700

    typing

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,5 @@
-var a = "1";
-console.log("aaa", a);
+var hello = "world";
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("true", "true"));
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
-const a = "1"
-console.log ("aaa", a)
+let hello: string = "world"
+
+const getFullName = (name: string, surname: string): string => {
+  return `${name} ${surname}`
+}
+
+
+console.log (getFullName ("true", "true"))
